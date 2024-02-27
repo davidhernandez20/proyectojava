4c58bf6005bae0259118376a89f95a23a116d512
davidhernandez20 <luisdavidhernandez57@gmail.com>
2024-02-27 11:30:20 -0500

    ok

---
+++ mydb.sql
@@ -0,0 +1,5 @@
+SELECT * FROM tienda;
+
+INSERT INTO tienda (id_tienda, id_productos, empleados,direccion, productos, nit, nombre_tienda, telefono, correo) VALUES ('1234','123','10','calle57#09sur','ropa deportiva','1234587980-1','comfecsport calle57', '1734657','confecsport43@gmail.com');
+
+
